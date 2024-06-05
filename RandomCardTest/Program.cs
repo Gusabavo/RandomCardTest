8b6318e671052735e349953d9a3b39997c0d66e3
Gusabavo <gustavo.sk.caxias@gmail.com>
2024-06-05 08:22:28 -0300

    Adicionar arquivos de projeto.

---
+++ RandomCardTest/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace RandomCardTest
+{
+    class Program
+    {
+        private static readonly Random random = new Random();
+        static Card RandomCard()
+        {
+            return new Card((Values)random.Next(1, 14), (Suits)random.Next(4));
+        } 
+
+        static void PrintCards(List<Card> cards)
+        {
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.Name);
+            }
+        }
+       
+        static void Main(string[] args)
+        {
+            List<Card> cards = new List<Card>();
+            Console.WriteLine("Enter number od cards: ");
+            if (int.TryParse(Console.ReadLine(), out int numberOfCards))
+                for (int i = 0; i < numberOfCards; i++) 
+                    cards.Add(RandomCard());
+            PrintCards(cards);
+
+            cards.Sort(new CardComparerByValue());
+            Console.WriteLine("\n... sorting the cards ...\n");
+            
+            PrintCards(cards);
+        }
+    }
+}
